6a3a407d66381e0b66d58099b817abee34b856d2
Délita Makanda <delita.makanda@gmail.com>
2026-04-13 18:34:15 +0200

    Make `checkoutAccessGuard` use relative navigation with `ActivatedRoute`

--- src/app/core/guards/checkout-access.guard.ts
+++ src/app/core/guards/checkout-access.guard.ts
@@ -1,12 +1,14 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { ActivatedRoute, CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { CartStore } from '../../state/cart.store';
 
 export const checkoutAccessGuard: CanActivateFn = () => {
   const cartStore = inject(CartStore);
   const router = inject(Router);
+  const route = inject(ActivatedRoute);
+
   if(!cartStore.items().length) {
-    return router.createUrlTree(['../menu'])
+    return router.createUrlTree(['../menu'], { relativeTo: route }  );
   }
   return true;
 };
