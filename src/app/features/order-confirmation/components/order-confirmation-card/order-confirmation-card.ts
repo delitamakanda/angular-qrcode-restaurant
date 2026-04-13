5f1c254cce2d56847120a489048e066761b37665
Délita Makanda <delita.makanda@gmail.com>
2026-04-13 18:35:16 +0200

    Add Order Confirmation Card component with standalone setup
    - Created `OrderConfirmationCard` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/order-confirmation/components/order-confirmation-card/order-confirmation-card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-confirmation-card',
+  imports: [],
+  templateUrl: './order-confirmation-card.html',
+  styleUrl: './order-confirmation-card.css',
+  standalone: true,
+})
+export class OrderConfirmationCard {}
