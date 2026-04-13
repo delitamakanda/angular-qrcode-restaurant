2ddd23b3959f36da83558a3d7faa60abfec276e8
Délita Makanda <delita.makanda@gmail.com>
2026-04-13 18:33:24 +0200

    Add Order Tracking Page component with standalone setup
    - Implemented `OrderTrackingPage` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/order-tracking/pages/order-tracking-page/order-tracking-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-tracking-page',
+  imports: [],
+  templateUrl: './order-tracking-page.html',
+  styleUrl: './order-tracking-page.css',
+})
+export class OrderTrackingPage {}
