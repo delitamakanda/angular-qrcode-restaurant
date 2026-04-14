59d1c6c368d9f3f443be9b9f3d33894515d53e81
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:27:39 +0200

    Add CheckoutPaymentMethods component with standalone setup
    - Created `CheckoutPaymentMethods` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/checkout/components/checkout-payment-methods/checkout-payment-methods.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-payment-methods',
+  imports: [],
+  templateUrl: './checkout-payment-methods.html',
+  styleUrl: './checkout-payment-methods.css',
+})
+export class CheckoutPaymentMethods {}
