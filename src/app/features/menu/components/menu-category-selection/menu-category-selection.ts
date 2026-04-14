82a8180c88dfd2b4dd5059dd3685e898352859d7
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:31:11 +0200

    Add MenuCategorySelection, MenuList, and MobileOrderLayout components with standalone setup
    - Created `MenuCategorySelection`, `MenuList`, and `MobileOrderLayout` as standalone Angular components.
    - Added basic HTML structure, CSS files, and component setup with `@Component` decorator.

---
+++ src/app/features/menu/components/menu-category-selection/menu-category-selection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-category-selection',
+  imports: [],
+  templateUrl: './menu-category-selection.html',
+  styleUrl: './menu-category-selection.css',
+})
+export class MenuCategorySelection {}
---
+++ src/app/layouts/mobile-order-layout/mobile-order-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-order-layout',
+  imports: [],
+  templateUrl: './mobile-order-layout.html',
+  styleUrl: './mobile-order-layout.css',
+})
+export class MobileOrderLayout {}
