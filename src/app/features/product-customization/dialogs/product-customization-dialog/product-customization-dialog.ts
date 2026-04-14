dac89fa2ef433102b8f371403418207ef0572940
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:29:24 +0200

    Add ProductCustomizationDialog component with standalone setup
    - Created `ProductCustomizationDialog` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/product-customization/dialogs/product-customization-dialog/product-customization-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-customization-dialog',
+  imports: [],
+  templateUrl: './product-customization-dialog.html',
+  styleUrl: './product-customization-dialog.css',
+})
+export class ProductCustomizationDialog {}
