3cc951188bcbafcc21928ff8d3fdcd7661d47f21
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:29:34 +0200

    Add ProductCustomization component with standalone setup
    - Created `ProductCustomization` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/product-customization/product-customization.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-customization',
+  imports: [],
+  templateUrl: './product-customization.html',
+  styleUrl: './product-customization.css',
+})
+export class ProductCustomization {}
