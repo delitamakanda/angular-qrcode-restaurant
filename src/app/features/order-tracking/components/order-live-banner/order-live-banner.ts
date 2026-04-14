02f4daad47e3ffbc925ed70745502fefe1446720
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:31:28 +0200

    Add OrderLiveBanner component with standalone setup
    - Created `OrderLiveBanner` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/order-tracking/components/order-live-banner/order-live-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-live-banner',
+  imports: [],
+  templateUrl: './order-live-banner.html',
+  styleUrl: './order-live-banner.css',
+})
+export class OrderLiveBanner {}
