3f87d3cb2f40ed085ae5506785f4289ca1b8debb
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 12:16:12 +0200

    Update package-lock.json with new dependencies
    - Added support for Taiga UI, Maskito, and @ng-web-apis libraries.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZonelessChangeDetection , provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideTaiga } from '@taiga-ui/core';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { loadingInterceptor} from './core/interceptors/loading.interceptor';
-import { errorInterceptor} from './core/interceptors/error.interceptor';
-import { localeInterceptor} from './core/interceptors/locale.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { localeInterceptor } from './core/interceptors/locale.interceptor';
 import { BASE_API_URL } from './core/config/app.token';
 
 import { routes } from './app.routes';
@@ -12,17 +23,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(
-      withInterceptors([
-        loadingInterceptor,
-        errorInterceptor,
-        localeInterceptor,
-      ])
-    ),
+    provideHttpClient(withInterceptors([loadingInterceptor, errorInterceptor, localeInterceptor])),
     {
       provide: BASE_API_URL,
-      useValue: '/api'
+      useValue: '/api',
     },
-    provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({ scrollPositionRestoration: 'enabled' , anchorScrolling: 'enabled'}), withPreloading(PreloadAllModules)),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' }),
+      withPreloading(PreloadAllModules),
+    ),
+    provideTaiga(),
   ],
 };
