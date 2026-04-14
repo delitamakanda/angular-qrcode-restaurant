9ff358260de2cf2351c0fc2748d867e0f48a5434
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:29:58 +0200

    Add OtpForm component with standalone setup
    - Created `OtpForm` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/auth/components/otp-form/otp-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-form',
+  imports: [],
+  templateUrl: './otp-form.html',
+  styleUrl: './otp-form.css',
+})
+export class OtpForm {}
