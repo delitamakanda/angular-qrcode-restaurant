17daebcc55ac3174b70f255279f767048941bbc1
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:29:45 +0200

    Add ProductBadge component with standalone setup
    - Created `ProductBadge` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/menu/components/product-badge/product-badge.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-badge',
+  imports: [],
+  templateUrl: './product-badge.html',
+  styleUrl: './product-badge.css',
+})
+export class ProductBadge {}
