e38b49ec83e81929244a037cead4c1bbfa4fd4a6
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:30:28 +0200

    Add OrderModeSelector component with standalone setup
    - Created `OrderModeSelector` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/welcome/components/order-mode-selector/order-mode-selector.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-mode-selector',
+  imports: [],
+  templateUrl: './order-mode-selector.html',
+  styleUrl: './order-mode-selector.css',
+})
+export class OrderModeSelector {}
