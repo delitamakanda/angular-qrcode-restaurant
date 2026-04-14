00bf07edbd449ba2f834f1ebb12b25b6e5871f5a
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:28:05 +0200

    Add CouponBanner component with standalone setup
    - Created `CouponBanner` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/promotions/components/coupon-banner/coupon-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupon-banner',
+  imports: [],
+  templateUrl: './coupon-banner.html',
+  styleUrl: './coupon-banner.css',
+})
+export class CouponBanner {}
