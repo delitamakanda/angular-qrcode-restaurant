5fbc2383bba65509e9e63d2b8c7bdc676c7cdbfc
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:29:10 +0200

    Add ProductNote component with standalone setup
    - Created `ProductNote` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/product-customization/components/product-note/product-note.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-note',
+  imports: [],
+  templateUrl: './product-note.html',
+  styleUrl: './product-note.css',
+})
+export class ProductNote {}
