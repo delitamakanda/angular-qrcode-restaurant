c7ad0139d1b52e4ea7c1270b7b46fd8b67fba335
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:28:35 +0200

    Add StoreHero component with standalone setup
    - Created `StoreHero` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/welcome/components/store-hero/store-hero.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-hero',
+  imports: [],
+  templateUrl: './store-hero.html',
+  styleUrl: './store-hero.css',
+})
+export class StoreHero {}
