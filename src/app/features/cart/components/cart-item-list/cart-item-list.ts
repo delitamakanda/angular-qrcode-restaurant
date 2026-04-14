8338628a82cb58953412ddbb2d7229ac813e294e
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:26:27 +0200

    Add CartItemList component with standalone setup
    - Created `CartItemList` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/cart/components/cart-item-list/cart-item-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item-list',
+  imports: [],
+  templateUrl: './cart-item-list.html',
+  styleUrl: './cart-item-list.css',
+})
+export class CartItemList {}
