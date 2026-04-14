8a065266c83209e5b8d298dcc7db061cfafbd11c
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:28:49 +0200

    Add PromoChip component with standalone setup
    - Created `PromoChip` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/promotions/components/promo-chip/promo-chip.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-chip',
+  imports: [],
+  templateUrl: './promo-chip.html',
+  styleUrl: './promo-chip.css',
+})
+export class PromoChip {}
