926d1ed4725ff1c360fc34975be6e4767d8bbab8
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:27:29 +0200

    Add CheckoutCoupon component with standalone setup
    - Created `CheckoutCoupon` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/checkout/components/checkout-coupon/checkout-coupon.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-coupon',
+  imports: [],
+  templateUrl: './checkout-coupon.html',
+  styleUrl: './checkout-coupon.css',
+})
+export class CheckoutCoupon {}
