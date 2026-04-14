d29b8e9556459d705888ce0c491918f951e474e4
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:27:06 +0200

    Add CenteredLayout component with standalone setup
    - Created `CenteredLayout` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/layouts/centered-layout/centered-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-centered-layout',
+  imports: [],
+  templateUrl: './centered-layout.html',
+  styleUrl: './centered-layout.css',
+})
+export class CenteredLayout {}
