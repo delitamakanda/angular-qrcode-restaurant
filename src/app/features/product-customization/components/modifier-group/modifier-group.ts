304262252de4c480e5e93e22b3071079270a74c0
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:30:47 +0200

    Add ModifierGroup component with standalone setup
    - Created `ModifierGroup` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/product-customization/components/modifier-group/modifier-group.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modifier-group',
+  imports: [],
+  templateUrl: './modifier-group.html',
+  styleUrl: './modifier-group.css',
+})
+export class ModifierGroup {}
