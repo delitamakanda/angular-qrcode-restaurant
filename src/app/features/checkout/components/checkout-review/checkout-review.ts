281298d146df98d999c5ea86dbd2e600d09560f6
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:27:50 +0200

    Add CheckoutReview component with standalone setup
    - Created `CheckoutReview` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/checkout/components/checkout-review/checkout-review.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-review',
+  imports: [],
+  templateUrl: './checkout-review.html',
+  styleUrl: './checkout-review.css',
+})
+export class CheckoutReview {}
