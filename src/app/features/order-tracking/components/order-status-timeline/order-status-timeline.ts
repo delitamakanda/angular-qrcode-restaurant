f06e91b8ad468479c6bb8206fe6567997028d4b2
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:30:13 +0200

    Add OrderStatusTimeline component with standalone setup
    - Created `OrderStatusTimeline` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/order-tracking/components/order-status-timeline/order-status-timeline.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-status-timeline',
+  imports: [],
+  templateUrl: './order-status-timeline.html',
+  styleUrl: './order-status-timeline.css',
+})
+export class OrderStatusTimeline {}
