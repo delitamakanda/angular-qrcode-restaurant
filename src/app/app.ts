7c798acf93db8ba556516dfb4ad70ef07cc95922
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 12:16:12 +0200

    Update package-lock.json with new dependencies
    - Added support for Taiga UI, Maskito, and @ng-web-apis libraries.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
-}
+export class App {}
