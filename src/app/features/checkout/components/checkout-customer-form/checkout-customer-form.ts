2fe72a1506652fa2596ecee168d1ad135e2f0b58
Délita Makanda <delita.makanda@gmail.com>
2026-04-14 10:27:18 +0200

    Add CheckoutCustomerForm component with standalone setup
    - Created `CheckoutCustomerForm` as a standalone Angular component.
    - Added basic HTML structure, CSS file, and component setup with `@Component` decorator.

---
+++ src/app/features/checkout/components/checkout-customer-form/checkout-customer-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-customer-form',
+  imports: [],
+  templateUrl: './checkout-customer-form.html',
+  styleUrl: './checkout-customer-form.css',
+})
+export class CheckoutCustomerForm {}
