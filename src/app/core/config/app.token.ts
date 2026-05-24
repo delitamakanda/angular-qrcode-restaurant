e2a2e2ab967b15c73a4a997a1048eff2545ead21
Délita Makanda <delita.makanda@gmail.com>
2026-05-24 23:11:11 +0200

    feat: implement API base URL injection token and environment configurations

--- src/app/core/config/app.token.ts
+++ src/app/core/config/app.token.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
+import { ApiConfig } from './env.config';
 
-export const BASE_API_URL = new InjectionToken<string>('BASE_API_URL');
+export const APP_TOKEN_CONFIG = new InjectionToken<ApiConfig>('APP_TOKEN');
