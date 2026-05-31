85cf264e232c0ab96a1a1c4d6e9d467d208b66ae
Délita Makanda <delita.makanda@gmail.com>
2026-05-31 20:52:52 +0200

    Treat empty array selections as invalid

--- src/app/shared/validators/required-selection.validator.ts
+++ src/app/shared/validators/required-selection.validator.ts
@@ -3,6 +3,6 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export function requiredSelectionValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value as unknown;
-    return value ? null : { requiredSelection: true };
+    return value && (value as unknown[]).length > 0 ? null : { requiredSelection: true };
   };
 }
